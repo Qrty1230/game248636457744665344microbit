3e6811d26efa0f54540c95413b8e763c9ab32e5d
Qrty1230 <yanis.l.sergeevich@gmail.com>
2025-11-23 17:25:45 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+function fallingfromtheskygame () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    led.plot(fallfromtheskygamerandomnumberbetween1and5, 0)
+    basic.pause(400)
+    led.plot(fallfromtheskygamerandomnumberbetween1and5, 1)
+    basic.pause(400)
+    led.plot(fallfromtheskygamerandomnumberbetween1and5, 2)
+    basic.pause(400)
+    led.plot(fallfromtheskygamerandomnumberbetween1and5, 3)
+    basic.pause(400)
+    led.plot(fallfromtheskygamerandomnumberbetween1and5, 4)
+    basic.pause(100)
+}
 function abalert () {
     basic.showLeds(`
         . . . . .
@@ -19,7 +38,7 @@ function abalert () {
     basic.clearScreen()
 }
 function loading () {
-    basic.pause(500)
+    basic.pause(100)
     music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
     basic.showLeds(`
         . . # . .
@@ -28,7 +47,7 @@ function loading () {
         # . # . #
         . . # . .
         `)
-    basic.pause(500)
+    basic.pause(100)
     music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
     basic.showLeds(`
         . # . # .
@@ -37,7 +56,7 @@ function loading () {
         . . # . .
         . # . # .
         `)
-    basic.pause(500)
+    basic.pause(100)
     music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
     basic.showLeds(`
         . . . . .
@@ -46,7 +65,7 @@ function loading () {
         # . # . #
         . . . . .
         `)
-    basic.pause(500)
+    basic.pause(100)
     music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
     for (let index = 0; index < 3; index++) {
         basic.showLeds(`
@@ -56,7 +75,7 @@ function loading () {
             . . # . .
             . # . # .
             `)
-        basic.pause(500)
+        basic.pause(100)
         music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
         basic.showLeds(`
             . . # . .
@@ -65,7 +84,7 @@ function loading () {
             . # . # .
             . . # . .
             `)
-        basic.pause(500)
+        basic.pause(100)
         music.play(music.stringPlayable("- A C G E B C5 D ", 300), music.PlaybackMode.InBackground)
     }
     basic.showLeds(`
@@ -100,25 +119,121 @@ function loading () {
 }
 function menu () {
     abalert()
-    fallingfromtheskygame1()
+    fallfromtheskygamerandomnumberbetween1and5 = randint(1, 5)
+    fallingfromtheskygame()
 }
-function fallingfromtheskygame1 () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . # . .
-        `)
-    led.plot(1, fallfromtheskygamerandomnumberbetween1and5)
-    led.plot(2, fallfromtheskygamerandomnumberbetween1and5)
-    led.plot(3, fallfromtheskygamerandomnumberbetween1and5)
-    led.plot(4, fallfromtheskygamerandomnumberbetween1and5)
-    if (led.point(5, fallfromtheskygamerandomnumberbetween1and5)) {
-        led.unplot(4, fallfromtheskygamerandomnumberbetween1and5)
+input.onGesture(Gesture.TiltLeft, function () {
+    if (led.point(0, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            `)
+        basic.pause(400)
     }
-}
+    if (led.point(1, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(400)
+    }
+    if (led.point(2, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(400)
+    }
+    if (led.point(3, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            `)
+        basic.pause(400)
+    }
+    if (led.point(4, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        basic.pause(400)
+    }
+    if (led.point(5, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+        basic.pause(400)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    menu()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (led.point(0, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            `)
+    }
+    if (led.point(1, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    }
+    if (led.point(2, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            `)
+    }
+    if (led.point(3, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (led.point(4, 0)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+})
 let fallfromtheskygamerandomnumberbetween1and5 = 0
-fallfromtheskygamerandomnumberbetween1and5 = randint(1, 5)
 loading()
 menu()
