ce94cfce5d7553030b9ee4e31306fb2861b5a9a3
Qrty1230 <yanis.l.sergeevich@gmail.com>
2025-11-23 16:48:30 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,120 @@
+function abalert () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
+}
+function loading () {
+    basic.pause(500)
+    music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . # . .
+        # . # . #
+        . # . # .
+        # . # . #
+        . . # . .
+        `)
+    basic.pause(500)
+    music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . # . # .
+        . . # . .
+        # # . # #
+        . . # . .
+        . # . # .
+        `)
+    basic.pause(500)
+    music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        # . # . #
+        . . . . .
+        `)
+    basic.pause(500)
+    music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . # . # .
+            . . # . .
+            . # . # .
+            . . # . .
+            . # . # .
+            `)
+        basic.pause(500)
+        music.play(music.stringPlayable("- A C G E B C5 D ", 1000), music.PlaybackMode.InBackground)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(500)
+        music.play(music.stringPlayable("- A C G E B C5 D ", 300), music.PlaybackMode.InBackground)
+    }
+    basic.showLeds(`
+        . # . # .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+}
+function menu () {
+    abalert()
+    fallingfromtheskygame1()
+}
+function fallingfromtheskygame1 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    led.plot(1, fallfromtheskygamerandomnumberbetween1and5)
+}
+let fallfromtheskygamerandomnumberbetween1and5 = 0
+loading()
+menu()
+basic.forever(function () {
+    fallfromtheskygamerandomnumberbetween1and5 = randint(1, 5)
+})
